328c86f901e454fe13ef9b743c18c1ec980ecfa7
seongcheol <kyn04241@gmail.com>
2025-09-24 17:17:33 +0900

    조건문

---
+++ ch 1/src/control/IfEx3.java
@@ -0,0 +1,45 @@
+package control;
+
+import java.util.Scanner;
+
+public class IfEx3 {
+    public static void main(String[] args) {
+        // 중첩 if : if 블럭 내에 또 다른 if문 포함
+        // if (true) {
+        // if (condition) {
+
+        // } else {
+
+        // }
+        // }
+        // else{
+        // }
+        // 점수 입력 받기
+        // 90점보다 크거나 같으면 A 학점
+        // 90점 중에서 98점 이상은 A+ 학점
+        // 95 ~ 97점 : A0 학점
+        // 90 ~ 94점 : A- 학점
+
+        Scanner sc = new Scanner(System.in);
+        int jumsu = Integer.parseInt(sc.nextLine());
+        char grade = ' ', opt = '0';
+        if (jumsu >= 90) {
+            grade = 'A';
+            if (jumsu >= 98) {
+                opt = '+';
+            } else if (jumsu < 95) {
+                opt = '-';
+            }
+        } else if (jumsu >= 80) {
+            grade = 'B';
+            if (jumsu >= 88) {
+                opt = '+';
+            } else if (jumsu < 85) {
+                opt = '-';
+            }
+        } else {
+            grade = 'C';
+        }
+        System.out.println("학점 : " + grade + opt);
+    }
+}
